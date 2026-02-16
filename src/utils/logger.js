28ec5fd4a3d2e8657d996a4624259613e1eb3bba
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-02-15 23:26:49 -0700

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,11 @@
+📄 src/utils/logger.js
+A simple, timestamped logger with consistent formatting.
+
+`javascript
+export default {
+  info: (...msg) => console.log([INFO] ${new Date().toISOString()}, ...msg),
+  warn: (...msg) => console.warn([WARN] ${new Date().toISOString()}, ...msg),
+  error: (...msg) => console.error([ERROR] ${new Date().toISOString()}, ...msg)
+};
+`
+
